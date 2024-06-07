896c4811de8965a2cff319cadb9f7a20cf1e7354
HardyRak <119031361+HardyRak@users.noreply.github.com>
2024-06-07 16:37:36 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'android',
+  webDir: 'www'
+};
+
+export default config;
